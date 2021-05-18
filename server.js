460e412f5482da03a74170e6e91e8c593ae9161c
celestine <nwabuisicelestine@gmail.com>
2021-05-18 01:07:19 +0100

    created middleware to handle static files

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 //import modules
-const express = require('express');
-
-
-
-
+const express = require("express");
+const path = require("path");
 
 //create an instance of express
 const app = express();
 
-//create server 
+//middleware
+app.use(express.static(path.join(__dirname, "public")));
+
+//create server
 const Port = process.env.PORT || 3000;
 app.listen(Port, () => {
-    console.log(`Server is live on port ${Port}`);
-})
+	console.log(`Server is live on port ${Port}`);
+});
